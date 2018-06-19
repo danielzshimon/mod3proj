0d291ab7d7c4e1a232d9832fe3cc4f1128b2beeb
cjunks94 <cjunks94@gmail.com>
2018-06-19 11:12:17 -0400

    clean and moved to app.js

--- frontend_music_api/app.js
+++ frontend_music_api/app.js
@@ -1,12 +1,21 @@
+const api = new Adapter()
+const albumBox = document.querySelector(".album_box")
+
 function createAlbumDiv(album){
   return`<div data-album-id=${album.collectionId}>
     <h1>${album.collectionName}</h1>
     <img src="${album.artworkUrl100}" alt="">
     <h1>${album.artistName}</h1>
     <p>${album.collectionPrice}</p>
-    <p>------------------------------------------------</p>
+    <p>------------------------------------------------css</p>
     <br>
-
-
   </div>`
+}
+
+function appendAlbums(e){
+  api.searchAlbum(e).then(albums =>{
+    albums.results.forEach(album =>{
+      albumBox.innerHTML+=createAlbumDiv(album)
+    })
+  })
 }
--- frontend_music_api/index.js
+++ frontend_music_api/index.js
@@ -1,19 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
-  const api = new Adapter()
   const searchBar = document.querySelector("#search_form")
-  const albumBox = document.querySelector(".album_box")
 document.addEventListener('click',()=>{
-  console.log(api.searchAlbum('dookie'));
+///////func///////
 })
 document.addEventListener('submit', (e) =>{
-  e.preventDefault()
-  api.searchAlbum(e.target.album_search_input.value).then(albums =>{
-    albums.results.forEach(album =>{
-      console.log(album);
-
-      albumBox.innerHTML+=createAlbumDiv(album)
-    })
-  })
+  e.preventDefault()//no load
+  appendAlbums(e.target.album_search_input.value)
 })
 
 
